0b11c6fbffd39d5065300511457f103376585f18
liangxiong <scliangxiong@gmail.com>
2021-12-05 17:37:57 +0800

    add hu-tool dependency

---
+++ src/test/java/org/liangxiong/demo/TimeTest.java
@@ -0,0 +1,16 @@
+package org.liangxiong.demo;
+
+import cn.hutool.core.date.DateUtil;
+
+/**
+ * @author liangxiong
+ * @version 1.0.0
+ * @date 2021-12-04 16:09
+ * @description
+ **/
+public class TimeTest {
+
+    public static void main(String[] args) {
+        System.out.println(DateUtil.now());
+    }
+}
